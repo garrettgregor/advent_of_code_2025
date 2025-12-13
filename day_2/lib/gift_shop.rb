5907c0b54b8a391b2498c2ba2efcb64daab6a3de
Garrett Gregor <garrett.gregor@gmail.com>
2025-12-13 10:59:40 -0700

    feat: GiftShop#initialize and GiftShop#inputs

--- day_2/lib/gift_shop.rb
+++ day_2/lib/gift_shop.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class GiftShop
+  attr_reader :inputs, :part_2
+
+  def initialize(file_path, part_2: false)
+    @inputs = File.open(file_path).flat_map { |line| line.split(",") }
+    @part_2 = part_2
+  end
 end
