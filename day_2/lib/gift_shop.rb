c05561afe3402ae124d9316d83af4c7f40a0e755
Garrett Gregor <garrett.gregor@gmail.com>
2025-12-13 11:00:07 -0700

    feat: GiftShop#ranges

--- day_2/lib/gift_shop.rb
+++ day_2/lib/gift_shop.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class GiftShop
-  attr_reader :inputs, :part_2
+  attr_reader :inputs, :ranges, :part_2
 
   def initialize(file_path, part_2: false)
     @inputs = File.open(file_path).flat_map { |line| line.split(",") }
+    @ranges = @inputs.map do |input|
+      arr = input.split("-").map(&:to_i)
+      Range.new(arr[0], arr[1])
+    end
     @part_2 = part_2
   end
 end
