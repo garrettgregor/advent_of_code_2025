491a36b7ab8750eca5f9ba2d2eb6bb6ffff9af61
Garrett Gregor <garrett.gregor@gmail.com>
2025-12-13 11:08:16 -0700

    feat: GiftShop#invalid_ids

--- day_2/lib/gift_shop.rb
+++ day_2/lib/gift_shop.rb
@@ -3,10 +3,11 @@
 class GiftShop
   attr_reader :inputs, :ranges, :part_2
 
-  attr_accessor :invalid_id_sum
+  attr_accessor :invalid_id_sum, :invalid_ids
 
   def initialize(file_path, part_2: false)
     @inputs = File.open(file_path).flat_map { |line| line.split(",") }
+    @invalid_ids = []
     @invalid_id_sum = 0
     @ranges = @inputs.map do |input|
       arr = input.split("-").map(&:to_i)
