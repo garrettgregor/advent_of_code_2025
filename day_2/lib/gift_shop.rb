3b983f8d2e04a7bc17697e1c086d7bd031a529b5
Garrett Gregor <garrett.gregor@gmail.com>
2025-12-14 12:01:46 -0700

    refactor: day 2 part 1

--- day_2/lib/gift_shop.rb
+++ day_2/lib/gift_shop.rb
@@ -9,24 +9,41 @@ class GiftShop
     @inputs = File.open(file_path).flat_map { |line| line.split(",") }
     @invalid_ids = []
     @ranges = @inputs.map do |input|
-      arr = input.split("-").map(&:to_i)
-      Range.new(arr[0], arr[1])
+      parts = input.split("-")
+      Range.new(parts[0].to_i, parts[1].to_i)
     end
     @part_2 = part_2
   end
 
   def find_invalid_ids(range)
-    range.each do |num|
-      first_half = num.to_s[0..((num.to_s.length / 2) - 1)]
-      second_half = num.to_s[((num.to_s.length / 2))..]
+    (range.begin.to_s.length..range.end.to_s.length).each do |len|
+      next unless len.even?
 
-      @invalid_ids << num if first_half == second_half && !first_half.start_with?("0")
+      generate_candidates_for_length(len, range)
+    end
+  end
+
+  def generate_candidates_for_length(len, range)
+    half_len = len / 2
+    start_half = 10**(half_len - 1)
+    end_half = (10**half_len) - 1
+    find_valid_half_strings(start_half, end_half, range.begin, range.end)
+  end
+
+  def find_valid_half_strings(start_half, end_half, min, max)
+    (start_half..end_half).each do |half|
+      s = half.to_s
+      full = (s + s).to_i
+      break if full > max
+      next if full < min
+
+      @invalid_ids << full
     end
   end
 
   def part_1
     @ranges.each { |range| find_invalid_ids(range) }
 
-    @invalid_ids.sum
+    @invalid_ids.uniq.sum
   end
 end
